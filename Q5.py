fca37173e937d246db60a6922dcae6b747283737
Anasha <a.nasha.wani.03@gmail.com>
2025-04-23 17:55:00 +0530

    Update Q5.py

--- Q5.py
+++ Q5.py
@@ -3,7 +3,7 @@
 list=[4,0.46,77,-3,94,-77,12,3,-333]
 
 # The list can be sort in either ascending or descending order.
-# If reverse=False, soring will be ascending , If reverse=True, sorting will be descending.
+# If reverse=False, sorting will be ascending , If reverse=True, sorting will be descending.
 
 list.sort(reverse = False)
 print(list)
